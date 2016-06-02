ee027ca247af7785bede309f85c4d6a7a70f3c93
cfcrawford <camilafcrawford@gmail.com>
2016-06-01 20:09:48 -0700

    Adds initial SQL file

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,18 +1,22 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-
+SELECT * FROM country WHERE region LIKE '%Southern Europe%' ORDER BY population ASC;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+select "language", isofficial from countrylanguage where countrycode like '%VAT%';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+#finds percentage of italian spoken per country code
+select countrycode, percentage from countrylanguage where language like '%Italian%';
+#finds city name from country code
+select name from city where countrycode like '%SMR%';
 
 
 
@@ -20,19 +24,21 @@
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
+SELECT * FROM city WHERE countrycode='SMR';
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+-- from city where name is like (first part of word)
+select * from city where name like '%Serra%';
+select name from country where code like '%BRA%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+select * from city where countrycode like '%BRA%';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -48,7 +54,8 @@
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
-
+select name, population from city where population between 85000 and 91000;
+select * from city where population=91084;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
